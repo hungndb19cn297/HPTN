35cae23294c5f0c83087e16065376c74dd30dfb6
HungND <hungnd.b19cn297@stu.ptit.edu.vn>
2023-11-05 20:23:16 +0700

    build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,10 +3,11 @@ const nextConfig = {}
 
 module.exports = {
   nextConfig,
-  reactStrictMode: true,
+  reactStrictMode: false,
   env: {
     API_AUTH_URL: process.env.API_AUTH_URL,
     API_POST_URL: process.env.API_POST_URL,
     // DEFAULT_AVATAR: process.env.DEFAULT_AVATAR
-  }
+  },
+  typescript: { ignoreBuildErrors: true }
 }
